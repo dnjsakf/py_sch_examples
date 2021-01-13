05e2cff02cff2fc375d10dfc803acf11b50bed20
heoDolf <dnjsakf@gmail.com>
2021-01-13 17:57:09 +0900

    Update

--- tester.py
+++ tester.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 from typing import (
   Optional, Union, Any, Callable, Iterable
 )
@@ -19,6 +20,8 @@ class BaseField(object):
   
   # 값 설정
   def setValue(self, value:Any, validate:Optional[bool]=True):
+    self.value = None
+    
     if validate:
       # 값이 있는 경우 타입체크
       if value is not None and not isinstance(value, self.type):
@@ -57,24 +60,35 @@ class StringField(BaseField):
   def validate(self, value=None):
     value = value or self.getValue()
     exists = value is not None
+    messages = self.getOption("messages", dict())
     
     # 유효성검사: 필수값
     if bool(self.required) and not exists:
-      raise ValueError("This value was Required, but it is None.")
+      msg = "This value was Required, but it is None."
+      msg = messages.get("required", msg)
+      raise ValueError(msg)
     
     # 유효성검사: 최대길이
     if bool(self.maxlength) and exists and len(value) > self.maxlength:
-      raise ValueError("Expacted value length %d, but %d." % ( self.maxlength, len(value) ))
+      msg = "Expacted value length %d, but %d." % ( self.maxlength, len(value) )
+      msg = messages.get("maxlength", msg)
+      raise ValueError(msg)
 
+      
 class BaseModel(object):
   def __init__(self, data:Optional[Union[dict,Iterable]]=None, **kwargs):
+    
+    self.__errors = None
+    
+    # Init Dumpping
     if data is not None:
       if isinstance(data, dict):
         self.dump(data)
       
       elif isinstance(data, Iterable) and hasattr(self, 'loads'):
         self.dumps(data)
-
+  
+  # 필드명, 필드클래스 가벼오기
   def getFields(self):
     fields = list()
     for field_name in dir(self):
@@ -85,21 +99,32 @@ class BaseModel(object):
     return fields
 
   # 값만 매핑
-  def dump(self, data:dict):
+  def dump(self, data:dict):  
     for field_name, field_class in self.getFields():
       value = data.get(field_name, None) or data.get(field_class.name, None)
       field_class.setValue(value, validate=False)
     return dict([(field_name, field_class.getValue()) for field_name, field_class in self.getFields()])
 
-  # 값을 매핑하면서 유효성검사까지
+  # 값을 매핑 && 유효성검사
   def load(self, data:dict):
+    self.__errors = list()
+  
     for field_name, field_class in self.getFields():
       value = data.get(field_name, None) or data.get(field_class.name, None)
-      field_class.setValue(value)
-    return dict([(field_class.name or field_name, field_class.getValue()) for field_name, field_class in self.getFields()])
+      try:
+        field_class.setValue(value)
+      except Exception as e:
+        self.__errors.append((field_name, str(e)))
+    
+    # 유효하지않은 값은 __errors에 저장하여 반환
+    loaded = dict([(field_class.name or field_name, field_class.getValue()) for field_name, field_class in self.getFields()])
+    loaded.update(__errors=dict(self.__errors))
+        
+    return loaded
 
   def __str__(self):
     return str(self.getFields())
+
     
 class ListModel(BaseModel):
   def __init__(self, *args, **kwargs):
@@ -146,14 +171,15 @@ class ListModel(BaseModel):
     else:
       raise StopIteration
 
+      
 class DummyModel(BaseModel):
   id = StringField(name="ID", required=True, maxlength=10)
   name = StringField(name="NAME", maxlength=10)
   
 class DummyListModel(ListModel):
   id = StringField(name="ID", required=True, maxlength=10)
-  name = StringField(name="NAME", maxlength=10)
-
+  name = StringField(name="NAME", required=True, maxlength=10, messages=dict(required="a"))
+  
 text1 = StringField(required=True, maxlength=10)
 text1.setValue("text1")
 
@@ -167,21 +193,26 @@ model1_data = model1.dump({
 model2 = DummyModel()
 
 list_model1 = DummyListModel()
+list_model1.loads([
+  {"id": "tesasdfasdfasdft"},
+  {"id": "test2"}
+])
+
 list_model2 = DummyListModel([
   {"id": "tesasdfasdfasdft"},
   {"id": "test2"}
 ])
 
-print( text1 )
-print( text2 )
+pprint( text1 )
+pprint( text2 )
 
-print( model1_data )
-print( model2 )
+pprint( model1_data )
+pprint( model2 )
 
-print( list_model1 )
+pprint( list_model1 )
 for data in list_model1:
-  print( data )
+  pprint( data )
 
-print( list_model2 )
+pprint( list_model2 )
 for data in list_model2:
-  print( data )
+  pprint( data )
